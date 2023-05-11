322731bbeff2db54651d887ae109820d95e20b55
Charles Lee <charlesmaclee@Charless-MBP.attlocal.net>
2023-05-10 17:22:25 -0700

    tailwind setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  return <div>Hello World</div>;
+  return <div className="text-3xl text-sky-500">Ketchup</div>;
 }
